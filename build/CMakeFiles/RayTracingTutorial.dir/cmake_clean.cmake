f4d2eb6878baad2e443025de20a425eaf52170e9
LucJRibas <l247231@dac.unicamp.br>
2022-08-01 12:26:26 -0300

    vscode integration

---
+++ build/CMakeFiles/RayTracingTutorial.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayTracingTutorial.dir/main.cpp.o"
+  "CMakeFiles/RayTracingTutorial.dir/main.cpp.o.d"
+  "RayTracingTutorial"
+  "RayTracingTutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayTracingTutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/RayTracingTutorial.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RayTracingTutorial.
+# This may be replaced when dependencies are built.
